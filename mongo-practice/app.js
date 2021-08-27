233d6c514d202c4fccb60969da89f06236ff9343
Mike DelGaudio <mdelgaud@stevens.edu>
2021-08-27 17:50:04 -0400

    adding other crud routes

--- mongo-practice/app.js
+++ mongo-practice/app.js
@@ -26,21 +26,61 @@ app.get("/todos", (req, res) => {
     });
 });
 
-app.put("todos/:id", (req, res) => {
+app.put("/todos/:id", (req, res) => {
   const todoId = req.params.id; // make sure valid int
   const userInput = req.body; // make sure valid
+  if (!todoId) {
+    return res.status(404).send("No id for todo provided");
+  }
+
+  if (!userInput) {
+    return res.status(404).send("Invalid body sent");
+  }
+
+  console.log(todoId);
+  console.log(userInput);
 
   db.getDb()
     .collection(collection)
     .findOneAndUpdate(
-      { _id: todoId },
-      { $set: { todo: userInput.todo } },
-      { returnOriginal: false },
+      { _id: db.getPrimaryKey(todoId) },
+      { $set: { todo: userInput?.todo } },
+      { returnOriginal: true },
       (err, result) => {
-        if (err) console.error("Failed to update I would send you a response");
-        else res.json(result);
+        if (err) {
+          console.error("Failed to update I would send you a response");
+        } else {
+          res.json(result);
+        }
       }
     );
+});
+
+app.post("/todos", (req, res) => {
+  const userInput = req.body;
+  db.getDb()
+    .collection(collection)
+    .insertOne(userInput, (err, result) => {
+      if (err) {
+        console.error(err);
+      } else {
+        res.json(result);
+      }
+    });
+});
+
+app.delete("/todos/:id", (req, res) => {
+  const todoId = req.params.id;
+
+  db.getDb()
+    .collection(collection)
+    .findOneAndDelete({ _id: db.getPrimaryKey(todoId) }, (err, result) => {
+      if (err) {
+        console.error(err);
+      } else {
+        res.json(result);
+      }
+    });
 });
 
 db.connect((err) => {
